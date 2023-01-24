6617ec0c29bc39049db53e559287fd4c7302bd0a
Muthukumar V <muthu@pmsquaresoft.com>
2023-01-24 07:22:39 +0530

    updated heroshot

--- src/HeroShot.jsx
+++ src/HeroShot.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import FoodServing from './images/restauranfood.jpg';
 
 const HeroShot = () => {
+  let navigate = useNavigate();
+  const gotoReserve = () => {
+    navigate('/reservations');
+  }
   return (
     <section className='flex flex-col-reverse items-center md:flex-row bg-gray-600'>
       <div className='flex flex-col w-[50%] justify-center ml-2 md:ml-6 pb-2 md:pb-4'>
@@ -11,7 +16,7 @@ const HeroShot = () => {
           We are a family owned mediterranean restaurant focussed on traditional
           recipes served with mordern twist..
         </p>
-        <input type='button' value='Reserve Table' className='btn-primary w-48 md:w-72' />
+        <input type='button' value='Reserve Table' className='btn-primary w-48 md:w-72' onClick={gotoReserve} />
       </div>
       <div className='flex justify-center w-full md:w-[50%] ml-2 md:ml-6'>
         <img className='w-72 h-96 rounded-lg md:w-48 md:h-64 m-4' src={FoodServing} alt='Food serving' />
